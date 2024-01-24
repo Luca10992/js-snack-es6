00664fa8f4ffc37335573a5a62812e4bb4c1e626
Luca10992 <lucalillo.99.1@gmail.com>
2024-01-24 15:39:05 +0100

    feat. add snack 1 e 2

--- js/main.js
+++ js/main.js
@@ -9,6 +9,29 @@
 // - posto occupato.
 // Generiamo e stampiamo in console l'array di oggetti per i segnaposto.
 
+const vips = [
+    'Dwayne Johnson',
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin',
+];
+
+const guestList = vips.map((vip, index) => {
+    return {
+        numberTable: 'Tavolo Vip',
+        guestName: vip,
+        seatNumber: index + 1,
+    }
+});
+console.log(guestList);
+
 // ?Snack 2:
 
 // Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame.
@@ -27,3 +50,52 @@
 // | 196 | Luigi Grimaldello     | 68     |
 // | 102 | Piero della Francesca | 50     |
 // | 120 | Francesca da Polenta  | 84     |
+
+const students = [
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grades: 78
+    },
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grades: 96
+    },
+    {
+        id: 250,
+        name: "Andrea Mantegna",
+        grades: 48
+    },
+    {
+        id: 145,
+        name: "Gaia Borromini",
+        grades: 74
+    },
+    {
+        id: 196,
+        name: "Luigi Grimaldello",
+        grades: 68
+    },
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50
+    },
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84
+    }
+]
+
+const nameUpperCase = students.map((student) => student.name.toUpperCase());
+console.log(nameUpperCase);
+
+const betterGrades = students.filter((student) => student.grades > 70);
+console.log("Gli studenti migliori sono: ", betterGrades);
+
+const listGradesId = students.filter((student) => student.grades > 70 && student.id > 120);
+console.log("Gli studenti con id maggiore di 120 e il voto più alto sono: ", listGradesId);
+
+
